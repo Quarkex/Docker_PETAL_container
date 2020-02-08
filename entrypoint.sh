613a802a74ae09decca315391524e45c8502a886
Manlio Joaquín García González <quarkex@pm.me>
2020-02-08 13:46:46 +0000

    Make entrypoint.sh replace itself

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd $APP_HOME
 if [ -f mix.exs ]; then
-  mix phx.server
+  exec mix phx.server
 else
   for folder in $(ls */); do
     if [ -f $folder/mix.exs ]; then
       cd $folder;
-      mix phx.server
+      exec mix phx.server
     fi
   done
 fi
