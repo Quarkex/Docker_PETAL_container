1b75d193b65af98f897bfa4e7bc44d2cb52723df
Manlio Joaquín García González <quarkex@pm.me>
2020-02-29 16:25:50 +0000

    Enable project scaffolding and management by variables

--- Dockerfile
+++ Dockerfile
@@ -7,17 +7,44 @@ MAINTAINER Manlio García <quarkex@gmail.com>
 
 RUN mix local.hex --force \
  && mix archive.install --force hex phx_new 1.4.12 \
- && apt-get update \
+ && apt-get update  \
+ && apt-get install \
+ locales            \
+ git                \
+ npm                \
+ apt-utils          \
+ nodejs             \
+ build-essential    \
+ inotify-tools      \
+ postgresql-client  \
+ -y                 \
  && curl -sL https://deb.nodesource.com/setup_13.x | bash \
- && apt-get install -y apt-utils \
- && apt-get install -y nodejs \
- && apt-get install -y build-essential \
- && apt-get install -y inotify-tools \
- && apt-get install -y postgresql-client \
  && mix local.rebar --force
 
+# Set the locale
+RUN touch /usr/share/locale/locale.alias
+RUN sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
+   locale-gen
+
+ENV LANG en_US.UTF-8
+ENV LANGUAGE en_US:en
+ENV LC_ALL en_US.UTF-8
+
 ENV APP_HOME /app
+ENV APP_NAME sample_app
+ENV TITLE sample_app
+ENV DOMAIN localhost
+ENV PORT 4000
+ENV MIX_ENV ""
+
+ENV PGPORT 5432
+ENV PGHOST 172.17.0.1
+ENV PGUSER postgres
+ENV PGPASSWORD postgres
+ENV PGDATABASE postgres
+
 RUN mkdir -p $APP_HOME
+COPY environment /etc/environment
 COPY entrypoint.sh /.
 WORKDIR $APP_HOME
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,88 @@
 #!/bin/bash
+
+export PGPORT="${DB_PORT:=$PGPORT}"
+export PGHOST="${DB_HOST:=$PGHOST}"
+export PGUSER="${DB_USER:=$PGUSER}"
+export PGPASSWORD="${DB_PASS:=$PGPASSWORD}"
+export PGDATABASE="${DB_NAME:=$PGDATABASE}"
+export DATABASE_URL="${DATABASE_URL:=ecto://$PGUSER:$PGPASSWORD@$PGHOST/$PGDATABASE}"
+export MIX_ENV="${MIX_ENV:=dev}"
+
 cd $APP_HOME
-if [ -f mix.exs ]; then
-  exec mix phx.server
-else
-  for folder in $(ls */); do
-    if [ -f $folder/mix.exs ]; then
-      cd $folder;
-      exec mix phx.server
+if [ ! -f mix.exs ]; then
+
+  if [ ! "$GIT_SOURCE" == "" ]; then
+    echo "Fetching phoenix project..."
+    git clone --recurse-submodules "$GIT_SOURCE" .
+  else
+    echo "Creating new phoenix project named “${APP_NAME}”..."
+
+    if [ "$DB_NAME" == "" ]; then
+      echo "y" | mix phx.new . --app "${APP_NAME}" --no-ecto &>/dev/null
+    else
+      echo "y" | mix phx.new . --app "${APP_NAME}" &>/dev/null
     fi
-  done
+
+    echo "Linking config values to env variables..."
+    sed -i \
+      -e 's/host: "[^"]*"/host: System.get_env("DOMAIN")/g' \
+        ./config/config.exs
+
+    sed -i \
+      -e 's/port: [0-9]*/port: String.to_integer(System.get_env("PORT"))/g' \
+        ./config/dev.exs
+
+    sed -i \
+      -e 's/host: "[^"]*"/host: System.get_env("DOMAIN")/g' \
+      -e 's/port: [0-9]*/port: String.to_integer(System.get_env("PORT"))/g' \
+        ./config/prod.exs
+
+    if [ ! "$DB_NAME" == "" ]; then
+      sed -i \
+        -e 's/username: "[^"]*",/username: System.get_env("PGUSER"),/g' \
+        -e 's/password: "[^"]*",/password: System.get_env("PGPASSWORD"),/g' \
+        -e 's/database: "[^"]*",/database: System.get_env("PGDATABASE")<>"_dev",/g' \
+        -e 's/hostname: "[^"]*",/hostname: System.get_env("PGHOST"),\n  port: String.to_integer(System.get_env("PGPORT")),/g' \
+          ./config/dev.exs
+
+      sed -i \
+        -e 's/port: [0-9]*/port: (String.to_integer(System.get_env("PORT")) + 2)/g' \
+        -e 's/username: "[^"]*",/username: System.get_env("PGUSER"),/g' \
+        -e 's/password: "[^"]*",/password: System.get_env("PGPASSWORD"),/g' \
+        -e 's/database: "[^"]*",/database: System.get_env("PGDATABASE")<>"_test",/g' \
+        -e 's/hostname: "[^"]*",/hostname: System.get_env("PGHOST"),\n  port: String.to_integer(System.get_env("PGPORT")),/g' \
+          ./config/test.exs
+    fi
+
+    sed -i \
+      -e 's;lang="[^"]*";lang="<%= System.get_env("LANGUAGE") %>";g' \
+      -e 's;<title>.*</title>;<title><%= System.get_env("TITLE") %></title>;g' \
+        ./lib/*_web/templates/layout/app.html.eex
+
+  fi
+
+  echo "Fetching elixir dependencies..."
+  mix deps.get &>/dev/null
+  echo "Fetching node dependencies..."
+  (cd assets && npm install &>/dev/null)
+
+  if [ "$SECRET_KEY_BASE" == "" ]; then
+    echo "Creating new secret..."
+    mix phx.gen.secret 128 | tail -n 1 >new_secret.erase_me.txt
+    export SECRET_KEY_BASE="$(cat new_secret.erase_me.txt | echo -n)"
+  fi
+
+  echo "Done"
 fi
+
+if [ "$MIX_ENV" == "prod" ]; then
+  mix deps.get --only "$MIX_ENV"
+  npm run deploy --prefix ./assets
+  mix phx.digest
+  if [ ! "$DB_NAME" == "" ]; then
+    mix ecto.setup
+    mix ecto.migrate
+  fi
+fi
+mix compile
+exec mix phx.server
